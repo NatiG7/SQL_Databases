7d860b93a5a858b427ab58f7decf7350b1e881e6
NatiG7 <NetanelGoral@gmail.com>
2024-12-15 17:28:36 +0200

    add: new queries added to queries.sh

--- MySQL/libraryDB/scripts/library_queries.sh
+++ MySQL/libraryDB/scripts/library_queries.sh
@@ -72,6 +72,61 @@ echo "Query : [ SELECT book_id, COALESCE(To_Date, "Not Returned") "Book Returned
 BOOKS_THAT_NOT_RETURNED="SELECT book_id, COALESCE(To_Date, 'Not Returned') 'Book Returned ?' From Borrowed;"
 $MYSQL_QUERY -e "$BOOKS_THAT_NOT_RETURNED"
 
+echo -e "Show 2 first names of customers that take CS"
+echo "Query : [ SELECT Cust_name FROM Customers WHERE Faculty='CS' LIMIT 1,2; ]"
+TWO_FIRST_NAMES_OF_CS="SELECT Cust_name FROM Customers WHERE Faculty='CS' LIMIT 0,2;"
+$MYSQL_QUERY -e "$TWO_FIRST_NAMES_OF_CS"
+
+echo -e "Show 2 books names, starting from the second book, less than pages 450"
+echo "Query : [ SELECT Book_name FROM Books WHERE Pages < 450 LIMIT 1,2; ]"
+SHOW_TWO_BOOKS_MORE_THAN_450_PAGE="SELECT Book_name FROM Books WHERE Pages < 450 LIMIT 1,2;"
+$MYSQL_QUERY -e "$SHOW_TWO_BOOKS_MORE_THAN_450_PAGE"
+
+echo -e "Show customer names ordered by ascending"
+echo "Query : [ SELECT Cust_name FROM Customers ORDER BY Cust_name; ]"
+SHOW_CUSTOMERS_ASC="SELECT Cust_name FROM Customers ORDER BY Cust_name;"
+$MYSQL_QUERY -e "$SHOW_CUSTOMERS_ASC"
+
+echo -e "Show customer names by faculty"
+echo "Query : [ SELECT Cust_name "Customer",Faculty FROM Customers ORDER BY Faculty; ]"
+SHOW_CUSTOMERS_BY_FAC="SELECT Cust_name "Customer",Faculty FROM Customers ORDER BY Faculty;"
+$MYSQL_QUERY -e "$SHOW_CUSTOMERS_BY_FAC"
+
+echo -e "Show names and page number of the 3 oldest books"
+echo "Query : [ SELECT Book_name "Book Name",Pages FROM Books ORDER BY Year LIMIT 0,3; ]"
+SHOW_NAMES_AND_PAGES_OLD_BOOKS_3="SELECT Book_name 'Book Name',Pages FROM Books ORDER BY Year LIMIT 0,3;"
+$MYSQL_QUERY -e "$SHOW_NAMES_AND_PAGES_OLD_BOOKS_3"
+
+echo -e "Show 3 book names with a max number of pages"
+echo "Query : [ SELECT Book_name,Pages FROM Books ORDER BY Pages DESC LIMIT 0,3; ]"
+SHOW_THREE_BOOKS_MOST_PAGES="SELECT Book_name,Pages FROM Books ORDER BY Pages DESC LIMIT 0,3;"
+$MYSQL_QUERY -e "$SHOW_THREE_BOOKS_MOST_PAGES"
+
+echo -e "Show all details of four newest books"
+echo "Query : [ SELECT * FROM Books ORDER BY Year DESC LIMIT 0,4; ]"
+FOUR_NEWSEST_BOOKS="SELECT * FROM Books ORDER BY Year DESC LIMIT 0,4;"
+$MYSQL_QUERY -e "$FOUR_NEWSEST_BOOKS"
+
+echo -e "Show calculated average pages of books containing 'System'"
+echo "Query : [ SELECT AVG(Pages) FROM Books WHERE Book_name LIKE '%System%'; ]"
+SHOW_BOOKS_AVG_CONTAIN_SYSTEM="SELECT FLOOR(AVG(Pages)) FROM Books WHERE Book_name LIKE '%System%';"
+$MYSQL_QUERY -e "$SHOW_BOOKS_AVG_CONTAIN_SYSTEM"
+
+echo -e "Show least pages for books from MED"
+echo "Query : [ SELECT MIN(Pages) FROM Books WHERE Faculty='MED'; ]"
+LEAST_PAGES_BOOKS_MED="SELECT MIN(Pages) FROM Books WHERE Faculty='MED';"
+$MYSQL_QUERY -e "$LEAST_PAGES_BOOKS_MED"
+
+echo -e "Show number of faculties that customers learn in"
+echo "Query : [ SELECT COUNT(DISTINCT Faculty) FROM Customers; ]"
+COUNT_FACULTIES_CUSTOMERS="SELECT COUNT(DISTINCT Faculty) FROM Customers;"
+$MYSQL_QUERY -e "$COUNT_FACULTIES_CUSTOMERS"
+
+echo -e "Show book names in the library, unique"
+echo "Query : [ SELECT DISTINCT(Book_name) FROM Books; ]"
+BOOK_NAMES_IN_LIBRARY="SELECT DISTINCT(Book_name) FROM Books;"
+$MYSQL_QUERY -e "$BOOK_NAMES_IN_LIBRARY"
+
 echo -e ""
 echo "Query : []"
 
